e65c44888fd54ddcbdb61cfc6ab49ea2dbe52db4
bjem <piotrworkkadula@gmail.com>
2019-01-05 20:18:47 +0100

    v3

--- react-netcore/ClientApp/src/components/Test.js
+++ react-netcore/ClientApp/src/components/Test.js
@@ -4,9 +4,6 @@ class Test extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
-            description: '',
-            items: [],
             isLoaded: false,
             search: ''
         }
